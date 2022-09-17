a2ef9342515eeea9e96747ca894a6eef39074cb7
meilun <1580429724@qq.com>
2022-09-17 22:48:58 +0800

    2022/9/4 MyBatis初次提交
    2022/9/5 MyBatis第二次提交，新建MyBatis1文件夹，用于练习RequestMap的一对多映射
    
    2022/9/6 算法学习初次提交，新建algorithmLearn1文件夹，用于练习算法相关问题
    
    2022/9/7 SpringBoot初次提交，新建SpringBoot文件夹
    
    2022/9/12 SpringBoot学习完成
    
    2022/9/13 mybatis_plus提交
    
    2022/9/17 SpringCloud初次提交
    
    2022/9/17 SpringCloud的Eureka和Zookeeper学习完成

---
+++ SpringCloud/cloud-consumerZK-order80/src/main/java/com/meilun/OrderMainZK80.java
@@ -0,0 +1,12 @@
+package com.meilun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderMainZK80 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMainZK80.class,args);
+    }
+}
--- SpringCloud/cloud-provider-payment8001/src/main/java/com/meilun/controller/PaymentController.java
+++ SpringCloud/cloud-provider-payment8001/src/main/java/com/meilun/controller/PaymentController.java
@@ -5,7 +5,11 @@ import com.meilun.entities.Payment;
 import com.meilun.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class PaymentController {
@@ -15,6 +19,9 @@ public class PaymentController {
 
     @Value("${server.port}")
     private String port;
+
+    @Autowired
+    DiscoveryClient discoveryClient;
 
 
     @PostMapping("/payment/create")
@@ -36,6 +43,23 @@ public class PaymentController {
         }else {
             return new CommentResult<>(444,"没有id对应的数据");
         }
+    }
+
+    @GetMapping("/payment/discovery")
+    public DiscoveryClient getTheDiscoveryClient(){
+
+        List<String> services = discoveryClient.getServices();
+        for (String service:services) {
+            System.out.println(service);
+        }
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("cloud-payment-service");
+
+        for (ServiceInstance instace: instances) {
+            System.out.println(instace.getServiceId()+" "+instace.getUri()+" "+instace.getMetadata());
+        }
+
+        return this.discoveryClient;
     }
 
 }
