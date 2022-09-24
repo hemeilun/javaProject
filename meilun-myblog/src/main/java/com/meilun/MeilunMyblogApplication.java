71bb0453024f27c502ad891ede22c832c9161076
meilun <1580429724@qq.com>
2022-09-24 17:49:15 +0800

    个人博客搭建
    首次提交
    
    前端基本完成
    
    导入依赖完成

--- meilun-myblog/src/main/java/com/meilun/MeilunMyblogApplication.java
+++ meilun-myblog/src/main/java/com/meilun/MeilunMyblogApplication.java
@@ -1,4 +1,4 @@
-package com.meilun.meilunmyblog;
+package com.meilun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- meilun-myblog/src/main/java/com/meilun/test/indexController.java
+++ meilun-myblog/src/main/java/com/meilun/test/indexController.java
@@ -1,4 +1,4 @@
-package com.meilun.meilunmyblog.test;
+package com.meilun.test;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
